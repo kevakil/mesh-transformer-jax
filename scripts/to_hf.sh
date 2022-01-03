8693a0e2155937435b0d3107214089164f385104
keyan <kevakil@github.com>
2022-01-02 19:59:16 -0800

    Add convert to hf format script

---
+++ scripts/to_hf.sh
@@ -0,0 +1,4 @@
+python3 to_hf_weights.py \
+--input-ckpt gs://aarrfsf/home/keyan/mesh-transformer-jax/split_training_data_model_dir/_slim/step_10 \
+--output-ckpt gs://aarrfsf/hf_model
+--config-path ./configs/split_training_data.json
