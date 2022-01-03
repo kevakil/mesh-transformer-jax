5fb42908d743faaf10e77d3c56ad41da779f4f9f
keyan <kevakil@github.com>
2022-01-02 17:03:54 -0800

    yolo

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-sudo python3 device_train.py --config=/home/keyan/mesh-transformer-jax/configs/split_training_data.json --tune-model-path=gs://aarrfsf/step_383500/
+python3 device_train.py --config=/home/keyan/mesh-transformer-jax/configs/split_training_data.json --tune-model-path=gs://aarrfsf/step_383500/
