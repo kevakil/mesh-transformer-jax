f013ea67eb8e347e56537bec450a7a04a9b04b12
keyan <kevakil@github.com>
2022-01-02 20:02:07 -0800

    wtf: :

--- scripts/to_hf.sh
+++ scripts/to_hf.sh
@@ -1,4 +1,1 @@
-python3 to_hf_weights.py \
---input-ckpt gs://aarrfsf/home/keyan/mesh-transformer-jax/split_training_data_model_dir/_slim/step_10 \
---output-ckpt gs://aarrfsf/hf_model \
---config-path ./configs/split_training_data.json
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/home/keyan/mesh-transformer-jax/split_training_data_model_dir/_slim/step_10 --output-ckpt gs://aarrfsf/hf_model --config-path ./configs/split_training_data.json
