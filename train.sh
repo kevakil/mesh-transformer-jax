6c1baec7f6ece70bd5493cd7a34a579663f7decd
K. Vakil <16906799+kevakil@users.noreply.github.com>
2022-01-02 16:04:25 -0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 device_train.py --config=/home/keyan/mesh-transformer-jax/configs/split_training_data.json --tune-model-path=gs://aarrfsf/step_383500/
+sudo python3 device_train.py --config=/home/keyan/mesh-transformer-jax/configs/split_training_data.json --tune-model-path=gs://aarrfsf/step_383500/
