1100bb706d3447fd1f0092de3f5eb69c6e79e18f
K. Vakil <16906799+kevakil@users.noreply.github.com>
2022-02-09 15:24:20 -0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 device_train.py --config=/home/keyan/mesh-transformer-jax/configs/transcripts.json --tune-model-path=gs://aarrfsf/step_383500/ --preemptible
+python3 device_train.py --config=/home/keyan/mesh-transformer-jax/configs/transcripts.json --tune-model-path=gs://aarrfsf/step_383500/
