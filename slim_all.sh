9f2ace50956abd98889a7d278a8c119997231ce8
K. Vakil <16906799+kevakil@users.noreply.github.com>
2022-02-10 20:04:47 -0800

    Create slim_all.sh

---
+++ slim_all.sh
@@ -0,0 +1,15 @@
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 406
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 379
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 352
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 325
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 298
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 271
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 244
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 217
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 190
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 163
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 136
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 109
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 82
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 55
+python3 slim_model.py --config configs/transcripts.json --ckpt-step 28
