68e0adcc72cbbe0acd12219be2f32d029b7eff11
K. Vakil <16906799+kevakil@users.noreply.github.com>
2022-02-10 20:11:50 -0800

    Create to_hf_all.sh

---
+++ to_hf_all.sh
@@ -0,0 +1,16 @@
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_432 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_406 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_379 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_352 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_325 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_298 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_271 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_244 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_217 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_190 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_163 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_136 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_109 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_82 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_55 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
+python3 to_hf_weights.py --input-ckpt gs://aarrfsf/transcripts/_slim/step_28 --config config/transcripts.json --output-path gs://aarrfsf/transcripts/hf/
